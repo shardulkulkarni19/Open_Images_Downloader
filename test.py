9f61c835f2f97b9f704e4186da8f089f685c83b3
Shardul Kulkarni <shardul.ask@gmail.com>
2020-07-24 12:30:47 +0530

    Updated with the latest code

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-#from pycocotools.coco import COCO
 import requests
 import os
 import pandas as pd
@@ -8,40 +7,40 @@ from datetime import datetime, timedelta
 folder_img_count = 500
 
 images = pd.read_csv(r'E:/Cemtrex Labs/Open Images/dummy.csv')
-img_name = images.iloc[:,0].tolist()
-img_url = images.iloc[:,1].tolist()
+img_name = images.iloc[:, 0].tolist()
+img_url = images.iloc[:, 1].tolist()
 # image = list(zip(img_name, img_url))
 
 total_images = len(img_name)
-#print(total_images)
-counter = 0
+# print(total_images)
+starting_index = 0
+counter = starting_index
 timer = datetime.now()
-for img,url in zip(img_name, img_url):
+for img, url in zip(img_name, img_url):
+    folder_name = 'open_data/open_v' + str(counter // folder_img_count)
+    filename = folder_name + '/' + img
+
+    if os.path.isfile(filename):
+        counter = +1
+        continue
+
+    print(counter, ": ", img)
+
     if counter % 50 == 0:
         tt = (datetime.now() - timer).seconds
-        print('time required for 50 images  ', tt, 'remaining time ',
-              (total_images - counter) * timedelta(seconds=tt / 50))
+        remaining_img = (starting_index + total_images - counter)
+        print('time required for 50 images  ', tt, 'remaining time ', remaining_img, 'Images',
+              remaining_img * timedelta(seconds=tt / 50))
         timer = datetime.now()
-    if counter < 0:
-        counter += 1
-        continue
-    print(counter, ": ", img)
-    ####### uncomment this when failure occurs #######
-    # if '000000385341.jpg' == img['file_name']:
-    #     quit()
-    ####### till here #######
 
-    ####### comment this when failure occurs #######
     img_data = requests.get(url).content
 
-    folder_name = 'open_data/open_v' + str(counter // folder_img_count)
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
-        #img_data = requests.get(url).content
+        # img_data = requests.get(url).content
 
-    filename = folder_name + '/' + img
     with open(filename, 'wb') as handler:
-         handler.write(img_data)
-#     ####### till here #######
+        handler.write(img_data)
+    #     ####### till here #######
     counter += 1
