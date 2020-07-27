c7ff97232d242ee21535cb9c833df0a1c0a1f632
kaleom410 <55679623+kaleom410@users.noreply.github.com>
2020-07-28 01:08:58 +0530

    config file

---
+++ config.py
@@ -0,0 +1,19 @@
+####### OPEN IMAGE DOWNLOAD CONFIGURATIONS#############
+
+
+TIMER_IMAGE_COUNT = 50
+FOLDER_IMG_COUNT = 500
+
+FOLDER_NAME ='O:/daaaCEMTREX/openImgCSv/v6/open_data/Val_occ_absent/open_v'
+
+STARTING_COUNTER = 0               # CHANGE THIS COUNTER ACCORDING TO SEQUENCE OF DATA
+
+SUCCESS_FILE = 'O:/daaaCEMTREX/openImgCSv/v6/open_data/test_success.csv'
+FAIL_FILE =    'O:/daaaCEMTREX/openImgCSv/v6/open_data/test_fail.csv'
+
+
+
+ANN_FILE = "O:/daaaCEMTREX/openImgCSv/v6/imageid-url/req_url/val_occ_absent_list.csv"###########ANNOTATION FILE
+
+THRESHOLD_IMG_SIZE = 1024
+
